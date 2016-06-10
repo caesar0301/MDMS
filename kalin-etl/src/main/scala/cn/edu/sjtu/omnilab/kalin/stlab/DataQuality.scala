74b5b20229f346cb6badbd262fcecb617d4dd19b
Xiaming Chen <chen@xiaming.me>
2016-06-10 23:33:35 +0800

    update

---
+++ kalin-etl/src/main/scala/cn/edu/sjtu/omnilab/kalin/stlab/DataQuality.scala
@@ -0,0 +1,11 @@
+package cn.edu.sjtu.omnilab.kalin.stlab
+
+/**
+ * Evaluate the quality of collected mobility data.
+ *
+ * Input format: (uid, time, lon, lat, time)
+ * Output format: (uid, q1, q2, ...)
+ */
+class DataQuality extends Serializable {
+
+}
